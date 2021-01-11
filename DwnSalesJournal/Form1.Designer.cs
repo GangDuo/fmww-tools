f3109f5f26ded8374259b7bdf58629b4b4d0b5ec
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-01-11 16:51:14 +0900

    POSデータを取得する

---
+++ DwnSalesJournal/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DwnSalesJournal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "売上データダウンロード中";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 85);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "進捗";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DwnSalesJournal/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Ctrls = Controls;
+
+namespace DwnSalesJournal
+{
+    /**
+     * usage : DwnSalesJournal -from -to -o filename
+     * DwnSalesJournal -f 2015/1/1 -t 2015/1/10 -o out.xlsx
+     * DwnSalesJournal -f 2015/1/1 -t 2015/1/10 -o out.xlsx --store-code 001
+     * DwnSalesJournal -f 2015/1/1 -t 2015/1/10 -o out.csv --as-csv
+     * 
+     * [required]
+     * -f : 売上期間　自
+     * -t : 売上期間　至
+     * -o : 保存先（絶対パス指定）
+     * 
+     * [Optional]
+     * --store-code : 店舗コード
+     * --as-csv : csvファイルとして出力する
+     * -g : 処理中のダイアログを表示する
+     */
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(Ctrl_C_Pressed);
+
+            var between = new FMWW.Component.Between<Nullable<DateTime>>();
+            var storeCode = "";
+            var asCsv = false;
+            var hasGui = false;
+            string o = "";
+            var args = Environment.GetCommandLineArgs();
+            #region コマンドラインオプションを設定する
+            var p = new Mono.Options.OptionSet() {
+                //オプションとオプションの説明、そのオプションの引数に対するアクションを定義する
+                {"c|store-code=", "店舗コード", v => storeCode = v},
+                {"a|as-csv", "結果をCSV形式で受取る", v => asCsv = v != null},
+                {"g|has-gui", "プログレスバーを表示する", v => hasGui = v != null},
+                {"o|output-file=", "エクセルの保存ファイルパス（拡張子:xlsxを含める）", v => o = v},
+                {"f|from=", "売上日", v => between.From = DateTime.Parse(v)},
+                {"t|to=", "売上日", v => between.To = DateTime.Parse(v)},
+            };
+            #endregion
+            #region parse command line.
+            List<string> extra;
+            //OptionSetクラスのParseメソッドでOptionSetの内容に合わせて引数をパースする。
+            //Paseメソッドはパース仕切れなかったコマンドラインのオプションの内容をstring型のリストで返す。
+            try
+            {
+                extra = p.Parse(args);
+                extra.ForEach(t => Debug.WriteLine(t));
+            }
+            //パースに失敗した場合OptionExceptionを発生させる
+            catch (Mono.Options.OptionException e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Console.WriteLine("Try `CommandLineOption --help' for more information.");
+                return;
+            }
+            #endregion
+            var context = new FMWW.ForShop.Work.Journals.Ref.Context()
+            {
+                PeriodOfSales = between,
+                StoreCode = storeCode,
+                AsCsv = asCsv
+            };
+            Task.Factory.StartNew(() =>
+            {
+                var task = FMWW.ForShop.Work.Journals.Ref.Downloader.DownloadTask(context);
+                task.Wait();
+                return task.Result;
+            })
+            .ContinueWith((task) =>
+            {
+                var filepath = task.Result;
+                try
+                {
+                    Console.WriteLine(filepath);
+                    if (asCsv)
+                    {
+                        Util.FileSystem.MoveFile(MsOffice.MsExcel.SaveAsCsv(filepath, false), o);
+                    }
+                    else
+                    {
+                        Util.FileSystem.MoveFile(MsOffice.MsExcel.SaveAs(filepath), o);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+//                    System.IO.File.Delete(filepath);
+                    Application.Exit();
+                }
+            }/*, TaskScheduler.FromCurrentSynchronizationContext()*/);
+
+            if (hasGui)
+            {
+                Application.Run(new Form1());
+            }
+            else
+            {
+                Application.Run();
+            }
+        }
+
+        // ［Ctrl］＋［C］キーが押されたときに呼び出される
+        private static void Ctrl_C_Pressed(object sender, ConsoleCancelEventArgs e)
+        {
+            //logger.Save(); // ログの保存
+        }
+    }
+}
