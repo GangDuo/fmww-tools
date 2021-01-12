0b897442f00ef42a5746773dd964a53ecf1cb531
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-01-12 16:16:32 +0900

    CSVを読み込んで、商品マスタを一括編集する

---
+++ ProductAlterationByCsv/ProductAlterationByCsv/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductAlterationByCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("csvファイルを指定してください");
+                return;
+            }
+            var csvFileShiftJIS = args[0];
+            if (!System.IO.File.Exists(csvFileShiftJIS))
+            {
+                Console.Error.WriteLine("csvファイルが見つからない");
+                return;
+            }
+            var task = FMWW.ExternalInterface.Products.Alteration.Uploader.UploadTask(csvFileShiftJIS);
+            task.Wait();
+            Console.Out.WriteLine(task.Result);
+        }
+    }
+}
---
+++ ProductAlterationByCsv/README.md
@@ -0,0 +1,6 @@
+﻿# CSVを読み込んで、商品マスタを一括編集する
+## 使用方法
+
+```powershell
+ProductAlterationByCsv.exe csv_file_path
+```
